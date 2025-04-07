c160be874043a409ce7f42fa6a0e51d4b16de61f
yoyoshingu <leo@shingu.ac.kr>
2025-04-07 14:25:54 +0900

    all 영화추천 완성

--- front/movie_js.js
+++ front/movie_js.js
@@ -1,10 +1,10 @@
 let MovieObject = {
     init: function(){
-        alert("init함수가 불려짐")
+
     },
 
     getall: function(){
-       alert("getall  함수가 불려짐")
+
        $.ajax({
        // 실행할 코드
           type: "GET",
@@ -15,28 +15,27 @@ let MovieObject = {
             movielist = response.result
 
             topdiv = document.createElement("div")
+            topdiv.style = "column-count:5"
             document.body.appendChild(topdiv)
 
-            // 첫번째 영화이미지
-            cmovie = document.createElement("div")
-            cmovie.className = "card"
-
-            mimg = document.createElement("img")
-            mimg.className = "card-img-top"
-            mimg.src = movielist[0].poster_path
-            cmovie.appendChild(mimg)
-            topdiv.appendChild(cmovie)
-
-            //두번째영화이미지
-            cmovie = document.createElement("div")
-            cmovie.className = "card"
-
-            mimg = document.createElement("img")
-            mimg.className = "card-img-top"
-            mimg.src = movielist[1].poster_path
-            cmovie.appendChild(mimg)
-            topdiv.appendChild(cmovie)
-
+            movielist.forEach( movie => {
+                cmovie = document.createElement("div")
+                cmovie.className = "card"
+    
+                mimg = document.createElement("img")
+                mimg.className = "card-img-top"
+                mimg.src = movie.poster_path
+                mimg.onclick = function(){
+                    // location.href = movie.url  // 현재창에서 열기
+                    window.open(movie.url)  // 새창에서 열기기
+                }
+                mimg.onmouseover = function(){
+                    
+                }
+                mimg.style.cursor = "pointer"
+                cmovie.appendChild(mimg)
+                topdiv.appendChild(cmovie)
+            });       
        }).fail(function(error){
            // 실패코드
            console.log(error)
